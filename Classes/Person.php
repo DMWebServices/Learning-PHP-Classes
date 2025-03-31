ac450adc6c66d466ea30297722a96e687f592b33
DM Web Services <138100708+DMWebServices@users.noreply.github.com>
2025-03-31 07:18:01 +0100

    Started to learn Objects

--- Classes/Person.php
+++ Classes/Person.php
@@ -3,13 +3,21 @@
 Class Person {
 
     // Properties / Fields
-    private $hairColour;
-    private $Eyecolour;
-    private $isHuman = "Yes";
+    private $haircolour;
+    private $eyecolour;
+    public $isHuman = "Yes";
 
     // Constructor
-    public function __construct() {
+    public function __construct($haircolour, $eyecolour = "None") {
+
+        $this->haircolour = $haircolour;
+        $this->eyecolour = $eyecolour;
         
     }
 }
+
+$person1 = new Person("Greeen", "Blue");
+$person2 = new Person("Red", "");
+
+echo $person1->isHuman;
 
